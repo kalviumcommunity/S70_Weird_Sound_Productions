c0943e6973286090ed0a86c05acf486e85f5aef8
Gautham ram <gauthamram.um@gmail.com>
2025-01-18 14:27:22 +0530

    nextsever

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/',(req,res)=>{
+    res.send({
+        "Routes":"/ping"
+    
+    })
+})
+
 app.get('/ping', (req,res) => {
     res.send('pong');
 });
