c9376d567c23c3969b385afbf54e04c858961038
Gautham Ram <gauthamram.um@gmail.com>
2025-02-07 09:31:33 +0530

    crud operation

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose')
-const connectDatabase = require('./db/Database'); 
-dotenv.config();
+const express = require("express");
+const connectDatabase = require("./db/Database"); 
+const itemRoutes = require("./routes/dataItem");
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-connectDatabase();
+connectDatabase(); 
+app.use(express.json()); 
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the MongoDB connection server!');
-});
-app.get('/status', (req, res) => {
-    if (mongoose.connection.readyState === 1) {
-        res.send('MongoDB connected successfully!');
-    } else {
-        res.status(500).send('MongoDB connection failed!');
-    }
-});
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.use("/item", itemRoutes); 
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
