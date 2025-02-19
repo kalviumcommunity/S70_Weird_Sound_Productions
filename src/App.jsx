e06e0c82a7f288f7470860a83c664c07da92c563
Gautham Ram <gauthamram.um@gmail.com>
2025-02-19 12:02:59 +0530

    landing page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './Components/landingPage';
+
+
+function App() {
+  return (
+  <BrowserRouter>
+  <Routes>
+  <Route path='/' element={<LandingPage/>} />
+  </Routes>
+</BrowserRouter>
+  )
+}
+
+export default App
